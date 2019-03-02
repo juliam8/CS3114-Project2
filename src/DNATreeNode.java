da203717af5549b72b0658cfe25501037e2e5b61
Julia Mahon Kuzin <juliam8@vt.edu>
2019-03-02 14:52:28 -0500

    lookie

---
+++ src/DNATreeNode.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package DNAtree;
+
+/**
+ * @author juliam8
+ * @author abbym1
+ * @version 2019-03-02
+ * @param 
+ * @param 
+ *
+ * Flyweight Node Object
+ * *description*
+ */
+public class DNATreeNode {
+    
+    
+    DNATreeNode(char[] dna){
+        sequence = dna;
+    }
+    
+    char[] sequence;
+    
+
+}
