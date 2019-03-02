b615da908b502b40094803bda1ae1b2797520c08
Julia Mahon Kuzin <juliam8@vt.edu>
2019-03-02 15:31:48 -0500

    flyweight

--- src/InternalNode.java
+++ src/InternalNode.java
@@ -10,5 +10,26 @@
  * *description*
  */
 public class InternalNode {
+    /**
+     * Constructor for LeafNode class
+     * @param sequence the DNA sequence for this leaf node
+     */
+    InternalNode(char[] sequence){
+        DNASequence = sequence;
+    }
+    
+    /**
+     * Returns the DNA sequence for this leaf node
+     * @return the DNA sequence
+     */
+    public char[] dna() {
+        return DNASequence;
+    }
+    
+    
+    /**
+     * Member variable that holds the DNA sequence of the node
+     */
+    private char[] DNASequence;
 
 }
--- src/LeafNode.java
+++ src/LeafNode.java
@@ -9,6 +9,28 @@
  * Leaf Node Object
  * *description*
  */
-public class LeafNode {
+public class LeafNode extends DNATreeNode{
+    
+    /**
+     * Constructor for LeafNode class
+     * @param sequence the DNA sequence for this leaf node
+     */
+    LeafNode(char[] sequence){
+        DNASequence = sequence;
+    }
+    
+    /**
+     * Returns the DNA sequence for this leaf node
+     * @return the DNA sequence
+     */
+    public char[] dna() {
+        return DNASequence;
+    }
+    
+    
+    /**
+     * Member variable that holds the DNA sequence of the node
+     */
+    private char[] DNASequence;
 
 }
